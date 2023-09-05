9d8c950e17c0f5510d176c446417857b8be0bdef
Mac&Cheese Grater2.0 <rory.ahanan181@gmail.com>
2023-09-05 16:02:01 +0100

    Update xxh64-generator.py
    Processes in 8kb chunks to reduce ram usage

--- xxh64-generator.py
+++ xxh64-generator.py
@@ -1,5 +1,6 @@
 import os
 import xxhash
+import json
 from tqdm import tqdm
 
 def generate_checksums(directory):
@@ -8,13 +9,16 @@ def generate_checksums(directory):
         for file in tqdm(files, desc="Hashing files", unit="file"):
             file_path = os.path.join(root, file)
             with open(file_path, "rb") as f:
-                checksum = xxhash.xxh64(f.read()).hexdigest()
+                hash_obj = xxhash.xxh64()
+                while True:
+                    chunk = f.read(8000)  # Read and hash in 8KB chunks
+                    if not chunk:
+                        break
+                    hash_obj.update(chunk)
+                checksum = hash_obj.hexdigest()
             relative_path = os.path.relpath(file_path, directory)
             checksums[relative_path] = checksum
     return checksums
-
-def compare_checksums(checksums_a, checksums_b):
-    return checksums_a == checksums_b
 
 if __name__ == "__main__":
     directory_a = input('Path A: ')
@@ -22,10 +26,14 @@ if __name__ == "__main__":
     
     checksums_a = generate_checksums(directory_a)
     checksums_b = generate_checksums(directory_b)
-
-    match = compare_checksums(checksums_a, checksums_b)
-
-    if match:
-        print("Checksums match.")
-    else:
-        print("Checksums do not match.")
+    
+    # Combine checksums and write to a JSON file
+    combined_checksums = {
+        "checksums_a": checksums_a,
+        "checksums_b": checksums_b
+    }
+    json_file_path = os.path.join(directory_b, "checksums.json")
+    with open(json_file_path, "w") as json_file:
+        json.dump(combined_checksums, json_file, indent=4)
+    
+    print("Checksums saved to:", json_file_path)
